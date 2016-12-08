08cf7b9ade28f44b250b56c18ab4a09c8bda5cfe
Kohányi Róbert <robert.kohanyi@codecool.com>
2016-12-08 10:21:38 +0000

    Rename main.py

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
-import main
+import batyu
 
 
 def test():
-    day_index = main.get_day_index()
-    monday_foods = main.get_menu(day_index)
+    day_index = batyu.get_day_index()
+    monday_foods = batyu.get_menu(day_index)
     assert len(monday_foods) > 0
 
-    sunday_foods = main.get_menu(6)
+    sunday_foods = batyu.get_menu(6)
     assert len(sunday_foods) == 0
 
 
